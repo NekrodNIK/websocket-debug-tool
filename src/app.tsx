7a4a4a5f3594cb359b4051f78ecba38ccb8b52e3
NekrodNIK <NP17210@yandex.ru>
2022-01-23 16:13:41 +0500

    add textarea

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./styles/app.scss"
 
 class App extends React.Component{
     render() {
         return (
-            <div>
-                Hello!
+            <div id="app_div">
+                <textarea></textarea>
+                <textarea></textarea>
             </div>
         );
     }
